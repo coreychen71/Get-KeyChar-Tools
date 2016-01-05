df87c60ddba045375b4dd627f49b5cf82c2bf008
CoreyChen <coreychen71@gmail.com>
2016-01-05 09:39:47 +0800

    Get KeyChar Tools

---
+++ KeyChar取得/MainProgram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KeyChar取得
+{
+    public partial class MainProgram : Form
+    {
+        public MainProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            label7.Text = e.KeyCode.ToString();
+            label8.Text = e.KeyValue.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            label6.Text = e.KeyChar.ToString();
+        }
+    }
+}
